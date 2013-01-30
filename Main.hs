de316a1259407c9ab73e8e8d84277282ffc5cf6f
Kranium Gikos Mendoza <kranium@gikos.net>
2013-01-30 22:26:12 +0800

    Use mfgno from the HTTP response.

--- Main.hs
+++ Main.hs
@@ -5,20 +5,20 @@ import Data.Maybe
 import qualified Data.Text as Text
 import Data.Time
 import Network.Browser
-import Network.CGI.Protocol (formEncode)
+import Network.CGI.Protocol (formEncode,formDecode)
 import Network.HTTP
+import Network.URI
 import Text.HTML.TagSoup
 
 data Account = Account {
   userid :: String,
-  passwd :: String,
-  mfgno :: String
+  passwd :: String
 } deriving (Show)
 
 u1 = "http://e-services.touchngo.com.my/e-Statement/"
 u2 = "http://e-services.touchngo.com.my/e-Statement/index.cfm"
 u3 = "http://e-services.touchngo.com.my/e-Statement/submit.cfm"
-u4 = (++) "http://e-services.touchngo.com.my/e-Statement/printstatementdetails.cfm?mfgno=" -- TODO: parse mfgno from u3 response
+u4 = (++) "http://e-services.touchngo.com.my/e-Statement/printstatementdetails.cfm?mfgno="
 
 getCSVLink [(TagOpen "a" [("href",url)])] = url
 getCSVLink _                              = error "CSV link not found"
@@ -39,8 +39,7 @@ getAccount :: Config -> IO Account
 getAccount config = do
   u <- Config.lookup config $ Text.pack "userid"
   p <- Config.lookup config $ Text.pack "passwd"
-  m <- Config.lookup config $ Text.pack "mfgno"
-  case Account <$> u <*> p <*> m  of
+  case Account <$> u <*> p of
     Just x -> return x
     _      -> error "error loading config"
 
@@ -71,7 +70,12 @@ main = do
                                         ,("toMonth",show m)
                                         ,("toYear",show y)]
 
-    (uri4,resp4) <- postReqWithParms (u4 (mfgno account)) [("exportCSV","Export to CSV")]
+    let uri3Params = (formDecode . query) uri3
+        mfgno = case (lookup "mfgno" uri3Params) of
+                     Just n  -> n
+                     _       -> error "mfgno not found"
+
+    (uri4,resp4) <- postReqWithParms (u4 mfgno) [("exportCSV","Export to CSV")]
 
     let csvLink = getCSVLink $ filter (~== "<a>") $ parseTags (rspBody resp4)
 
